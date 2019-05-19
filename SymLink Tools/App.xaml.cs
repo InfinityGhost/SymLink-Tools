d1be3962117db7bc6911e3ce8c3a1277ccaba4db
InfinityGhost <39218853+InfinityGhost@users.noreply.github.com>
2019-05-18 21:07:36 -0400

    Add command line arguments

--- SymLink Tools/App.xaml.cs
+++ SymLink Tools/App.xaml.cs
@@ -13,5 +13,17 @@ namespace SymLink_Tools
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            var args = e.Args;
+
+            var window = new MainWindow();
+            if (args.Length >= 1 && !string.IsNullOrWhiteSpace(args[0]))
+                window.RealPath = args[0];
+            if (args.Length >= 2 && !string.IsNullOrWhiteSpace(args[1]))
+                window.SymLinkPath = args[1];
+
+            MainWindow.Show();
+        }
     }
 }
